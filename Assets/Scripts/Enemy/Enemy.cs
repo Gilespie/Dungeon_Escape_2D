5c32bb53d10441bc382b4b8dfd2f0c84a51b9449
Gilespie <pierreraider@gmail.com>
2023-09-26 19:32:14 -0300

    Changes #5
    Added scripts Enemy, Spider, MossGiant.
    Added animations for Moss_Giant such as Attack, Death, Hit, Idle, Walk.
    Added animation for Skeleton such as Attack, Death, Hit, Idle, Walk.
    Added animations for Spider such as Attack, Death, Idle, Walk.
    Added subdata in Scripts such as Player, Enemy.
    
    Changed and sliced sprites Skeleton, Moss_Giant and Spider.
    
    Moved scripts Player and PlayerAnimation from data Scripts to subdata Player.

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    [SerializeField] protected int health;
+    [SerializeField] protected int speedMovement;
+    [SerializeField] protected int gems;
+    [SerializeField] protected Transform pointA, pointB;
+
+    protected Vector3 currentTarget;
+    protected Animator animator;
+    protected SpriteRenderer sprite;
+
+    private void Start()
+    {
+        Init();
+    }
+
+    protected virtual void Update()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle") == true) return;
+
+        Movement();
+        FlipSprite();
+    }
+
+    protected virtual void Init()
+    {
+        animator = GetComponentInChildren<Animator>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    protected virtual void Movement()
+    {
+        if (transform.position == pointA.position)
+        {
+            currentTarget = pointB.position;
+            animator.SetTrigger("Idle");
+        }
+        else if (transform.position == pointB.position)
+        {
+            currentTarget = pointA.position;
+            animator.SetTrigger("Idle");
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, currentTarget, speedMovement * Time.deltaTime);
+    }
+
+    protected virtual void FlipSprite()
+    {
+        if (currentTarget == pointA.position)
+        {
+            sprite.flipX = true;
+        }
+        else if (currentTarget == pointB.position)
+        {
+            sprite.flipX = false;
+        }
+    }
+
+    protected virtual void Attack()
+    {
+
+    }
+}
