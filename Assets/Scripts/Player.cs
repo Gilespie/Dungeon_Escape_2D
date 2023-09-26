79a65cc3fcff5422770618a9a46405b9f33f2c9d
Gilespie <pierreraider@gmail.com>
2023-09-26 15:55:30 -0300

    Changes #4
    Imported Cinemachine.
    
    Added scripts Player, PlayerAnimation.
    Added animations Death, Fire_Swing, Hit, Idle, Jump, Jump_Swing, Jump_Swing_Fire, Run, Walk.
    
    Changed sprites Death,  Fire_Swing, Hit, Idle, Jump, Jump_Swing, Jump_Swing_Fire, Warrior_Run, Warrior_Walk and Sword_Three_Particle.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _speedMovement = 3f;
+    [SerializeField] private float _powerJump = 5f;
+
+    private float _rayDistance = 0.6f;
+    private bool _isGrounded = false;
+    private bool _resetJump = false;
+    private Rigidbody2D _rigidBody;
+    private PlayerAnimation _playerAnimation;
+    private SpriteRenderer _playerSprite;
+    private SpriteRenderer _swordSprite;
+
+    private void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+        _playerAnimation = GetComponent<PlayerAnimation>();
+        _playerSprite = GetComponentInChildren<SpriteRenderer>();
+        _swordSprite = transform.GetChild(1).GetComponent<SpriteRenderer>();
+    }
+
+    
+    private void Update()
+    {
+        Movement();
+
+        Attack();
+    }
+
+    private void Movement()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal") * _speedMovement;
+        _isGrounded = CheckGrounded();
+
+        Flip(horizontalInput);
+
+        if (Input.GetKeyDown(KeyCode.Space) && CheckGrounded() == true)
+        {
+            _rigidBody.velocity = new Vector2(_rigidBody.velocity.x, _powerJump);
+            _playerAnimation.Jump(true);
+            StartCoroutine(ResetJumpRoutine());
+        }
+
+        _rigidBody.velocity = new Vector2(horizontalInput, _rigidBody.velocity.y);
+
+        _playerAnimation.Move(horizontalInput);
+    }
+
+    private bool CheckGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, _rayDistance, 1 << 8);
+        Debug.DrawRay(transform.position, Vector2.down * _rayDistance, Color.red);
+
+        if (hit.collider != null)
+        {
+            if (_resetJump == false)
+            {
+                _playerAnimation.Jump(false);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void Attack()
+    {
+        if (Input.GetMouseButtonDown(0) && CheckGrounded() == true)
+        {
+            _playerAnimation.Attack();
+        }
+    }
+
+    private void Flip(float speedX)
+    {
+        if (speedX > 0)
+        {
+            _playerSprite.flipX = false;
+            _swordSprite.flipX = false;
+            _swordSprite.flipY = false;
+
+            Vector3 newPos = _swordSprite.transform.localPosition;
+            newPos.x = 1.01f;
+            _swordSprite.transform.localPosition = newPos;
+        }
+        else if (speedX < 0)
+        {
+            _playerSprite.flipX = true;
+            _swordSprite.flipX = true;
+            _swordSprite.flipY = true;
+
+            Vector3 newPos = _swordSprite.transform.localPosition;
+            newPos.x = -1.01f;
+            _swordSprite.transform.localPosition = newPos;
+        }
+    }
+
+    private IEnumerator ResetJumpRoutine()
+    {
+        _resetJump = true;
+        yield return new WaitForSeconds(0.1f);
+        _resetJump = false;
+    }
+}
